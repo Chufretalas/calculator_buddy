1e7c8cb1bd925468ca93856daed9e75db02a85e9
Chufretalas <marcoantonio.bene@gmail.com>
2025-01-23 16:34:21 -0300

    everything is working on the most basic level

--- main.py
+++ main.py
@@ -1,24 +1,75 @@
 import subprocess
+import os
+from time import sleep
 import pyautogui as pgui
 
 # https://github.com/asweigart/PyGetWindow/issues/36
 from pygetwindow import getWindowsWithTitle
 
-from time import sleep
+
+def test_all():
+    for img in os.listdir("./buttons"):
+        try:
+            buttonx, buttony = pgui.locateCenterOnScreen(
+                f"./buttons/{img}", confidence=0.9
+            )
+            pgui.click(buttonx, buttony)
+        except pgui.ImageNotFoundException as e:
+            print(f"Did not find {img}")
+            print(e)
+
+
+def click_button(file_name: str):
+    try:
+        buttonx, buttony = pgui.locateCenterOnScreen(
+            f"./buttons/{file_name}", confidence=0.9
+        )
+        pgui.click(buttonx, buttony)
+    except pgui.ImageNotFoundException as e:
+        print(f"Did not find {file_name}")
+        print(e)
 
 
 def main():
     calcProcess = subprocess.Popen(r"C:\Windows\System32\calc.exe")
 
-    sleep(1)
+    sleep(0.2)
+
+    commands: str = pgui.prompt(
+        "Please type desired sequence of commands. Use * and /  for multiplication and division. Use a comma as the decimal separator."
+    )
+
+    sleep(0.1)
 
     calc_window = getWindowsWithTitle("Calculadora")[0]
     if not calc_window.isActive:
         pgui.press("altleft")
         calc_window.activate()
 
-    buttonx, buttony = pgui.locateCenterOnScreen("./buttons/8.png")
-    pgui.click(buttonx, buttony)
+    sleep(0.1)
+
+    for c in commands:
+        if c.isnumeric():
+            click_button(f"{c}.png")
+            continue
+        match c:
+            case ",":
+                click_button("comma.png")
+
+            case "/":
+                click_button("div.png")
+
+            case "=":
+                click_button("equals.png")
+
+            case "-":
+                click_button("minus.png")
+
+            case "*":
+                click_button("mul.png")
+
+            case "+":
+                click_button("plus.png")
 
 
 if __name__ == "__main__":
