01ed922b83fb439c2c3a925e5845af289309e8a3
Chufretalas <marcoantonio.bene@gmail.com>
2025-01-23 00:24:16 -0300

    did some more testing and took all relevant button screeshots

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import subprocess
 import pyautogui as pgui
 
-def main():
-    calcPID = subprocess.Popen(r"C:\Windows\System32\calc.exe")
-    print(calcPID.pid)
+# https://github.com/asweigart/PyGetWindow/issues/36
+from pygetwindow import getWindowsWithTitle
 
-    screenWidth, screenHeight = pgui.size()
+from time import sleep
+
+
+def main():
+    calcProcess = subprocess.Popen(r"C:\Windows\System32\calc.exe")
+
+    sleep(1)
+
+    calc_window = getWindowsWithTitle("Calculadora")[0]
+    if not calc_window.isActive:
+        pgui.press("altleft")
+        calc_window.activate()
+
+    buttonx, buttony = pgui.locateCenterOnScreen("./buttons/8.png")
+    pgui.click(buttonx, buttony)
+
 
 if __name__ == "__main__":
     main()
